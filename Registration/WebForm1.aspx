b60696436e8055e6b7315ebc5a1e8ba5fe171502
MD Haque <ziacs2015@gmail.com>
2023-11-26 12:55:44 +0530

    Adding Design

--- Registration/WebForm1.aspx
+++ Registration/WebForm1.aspx
@@ -4,32 +4,146 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-    <title></title>
+   <script>
+       function showToast(type, message) {
+           var toastContainer = document.getElementById("toast-container");
+           var toast = document.createElement("div");
+           toast.className = "toast " + type;
+           toast.innerHTML = message;
+           toastContainer.appendChild(toast);
+
+           setTimeout(function () {
+               toast.remove();
+           }, 3000); // Adjust the timeout (in milliseconds) as needed
+       }
+   </script>
+ 
+    <title>Modern Registration Form</title>
+    <style>
+      
+         #toast-container {
+        position: fixed;
+        top: 20px;
+        right: 20px;
+        max-width: 300px;
+        z-index: 1000; /* Ensure it appears above other elements */
+    }
+
+    .toast {
+        background-color: #4CAF50; /* Green for success */
+        color: white;
+        padding: 15px;
+        margin-bottom: 10px;
+        border-radius: 5px;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    }
+
+    .error {
+        background-color: #f44336; /* Red for error */
+    }
+
+        body {
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+            background-color: #f8f9fa;
+            margin: 0;
+            padding: 0;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+        }
+
+        #form1 {
+            max-width: 400px;
+            background-color: #FDF7E4;
+            padding: 20px;
+            border-radius: 8px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            transition: box-shadow 0.3s ease;
+        }
+
+        #form1:hover {
+            box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
+        }
+
+        div {
+            margin-bottom: 20px;
+        }
+
+        label {
+            display: block;
+            margin-bottom: 8px;
+            color: #495057;
+            
+        }
+
+        .text-box {
+            width: calc(100% - 22px);
+            padding: 10px;
+            box-sizing: border-box;
+            border: 1px solid #ced4da;
+            border-radius: 4px;
+            transition: border-color 0.3s ease;
+        }
+
+        .text-box:focus {
+            border-color: #007bff;
+        }
+
+        .Button-div{
+            text-align: center;
+        }
+        .Button {
+            
+            background-color: #7B66FF;
+            color: #fff;
+            padding: 10px 20px;
+            border: 1px solid #7B66FF;
+            border-radius: 10px;
+            cursor: pointer;
+            transition: background-color 0.3s ease;
+           
+        }
+
+        .Button:hover {
+            background-color: white;
+            color:#7B66FF
+        }
+
+        .error-message {
+            color: #dc3545;
+            margin-top: 5px;
+        }
+    </style>
 </head>
 <body>
+    <div id="toast-container"></div>
     <form id="form1" runat="server">
-        <br />
-        Fill the Details.<br />
-        <br />
-        <br />
-        Full Name<div>
-            <asp:TextBox ID="FullName" runat="server" Width="193px"></asp:TextBox>
+        <h2>Employee Registration Form!</h2>
+        <div>
+            <label for="FullName">Full Name</label>
+            <asp:TextBox class="text-box" ID="FullName" runat="server"></asp:TextBox>
         </div>
-        <p>
-            Employee ID</p>
-        <asp:TextBox ID="Eid" runat="server" Width="193px"></asp:TextBox>
-        <p>
-            Department&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        </p>
-        <asp:TextBox ID="Dept" runat="server" Width="190px"></asp:TextBox>
-        <br />
-        <br />
-        Email ID<br />
-        <asp:TextBox ID="mail" runat="server" Width="188px"></asp:TextBox>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <p style="margin-left: 80px">
-            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Submit" />
-        </p>
+        <div>
+            <label for="Eid">Employee ID</label>
+            <asp:TextBox class="text-box" ID="Eid" runat="server"></asp:TextBox>
+        </div>
+        <div>
+            <label for="Dept">Department</label>
+            <asp:TextBox class="text-box" ID="Dept" runat="server"></asp:TextBox>
+        </div>
+        <div>
+            <label for="mail">Email ID</label>
+            <asp:TextBox class="text-box" ID="mail" runat="server"></asp:TextBox>
+           
+        </div>
+        <div class="Button-div">
+            <asp:Button class="Button" ID="Button1" runat="server" OnClick="Button1_Click" Text="Submit" />
+        </div>
     </form>
+     
+
+    
+     
 </body>
 </html>
--- Registration/WebForm1.aspx.cs
+++ Registration/WebForm1.aspx.cs
@@ -6,6 +6,7 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Data.SqlClient;
 using System.Data;
+using System.Configuration;
 
 namespace Registration
 {
@@ -24,7 +25,8 @@ namespace Registration
         protected void Button1_Click(object sender, EventArgs e)
         {
             // Replace "your_connection_string_here" with your actual database connection string
-            string connectionString = "Server=tcp:sqlappregtest.database.windows.net,1433;Initial Catalog=registration;Persist Security Info=False;User ID=sqluser;Password=Azure@12345;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            string connectionString = ConfigurationManager.ConnectionStrings["SqlConnectionString"].ConnectionString;
+            //string connectionString = "Server=tcp:empreg.database.windows.net,1433;Initial Catalog=Employee_Registration;Persist Security Info=False;User ID=sqluser;Password=Azure@12345;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
             try
             {
                     using (SqlConnection regcon = new SqlConnection(connectionString))
@@ -33,17 +35,30 @@ namespace Registration
 
                     using (SqlCommand insert = new SqlCommand("InsertEmployee", regcon))
                     {
+                        if (string.IsNullOrWhiteSpace(FullName.Text) || string.IsNullOrWhiteSpace(Eid.Text) || string.IsNullOrWhiteSpace(Dept.Text) || string.IsNullOrWhiteSpace(mail.Text))
+                        {
+                            // Display an error message using JavaScript
+                            Page.ClientScript.RegisterStartupScript(this.GetType(), "showError", "showToast('error', 'Please fill in all the fields.');", true);
+                            return;
+                        }
+                        if (IsEmailInUse(mail.Text, regcon))
+                        {
+                            // Display an error message using JavaScript
+                            Page.ClientScript.RegisterStartupScript(this.GetType(), "showError", "showToast('error', 'Email address is already in use. Please choose a different email.');", true);
+                            return;
+                        }
                         insert.CommandType = CommandType.StoredProcedure;
 
                         // Assuming you have variables for the values you want to insert
-                        insert.Parameters.AddWithValue("@emp_id", Eid.Text);
-                        insert.Parameters.AddWithValue("@full_name", FullName.Text);
-                        insert.Parameters.AddWithValue("@department", Dept.Text);
-                        insert.Parameters.AddWithValue("@email", mail.Text);
+                        insert.Parameters.AddWithValue("@EmployeeID", Eid.Text);
+                        insert.Parameters.AddWithValue("@FullName", FullName.Text);
+                        insert.Parameters.AddWithValue("@Department", Dept.Text);
+                        insert.Parameters.AddWithValue("@Email", mail.Text);
 
 
                         insert.ExecuteNonQuery();
-                        Response.Write("Employee inserted successfully!");
+                        string successScript = "showToast('success', 'Employee inserted successfully!');";
+                        ScriptManager.RegisterStartupScript(this, this.GetType(), "successScript", successScript, true);
                         ClearInputFields();
                         regcon.Close();
                     }
@@ -51,26 +66,48 @@ namespace Registration
             }
             catch (SqlException ex)
             {
-                // Check if the exception is due to a primary key violation
-                if (ex.Number == 2627 || ex.Number == 2601)
-                {
-                    // Handle the primary key violation error here
-                    // You can display a user-friendly message or log the error
-                    Response.Write("Employee ID already exists. Please choose a different ID.");
-                }
-                else
-                {
-                    // Handle other SQL exceptions
-                    // You can display a user-friendly message or log the error
-                    Response.Write("An error occurred: " + ex.Message);
-                }
+                // Your existing code
+
+                // Display error toast message
+                string errorScript = "showToast('error', '" + GetMessageBasedOnErrorCode(ex.Number) + "');";
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "errorScript", errorScript, true);
             }
             catch (Exception ex)
             {
-                // Handle other exceptions (not SQL-related)
-                // You can display a user-friendly message or log the error
-                Response.Write("An error occurred: " + ex.Message);
+                // Your existing code
+
+                // Display error toast message
+                string errorScript = "showToast('error', 'An error occurred: " + ex.Message + "');";
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "errorScript", errorScript, true);
             }
+        }
+
+        private bool IsEmailInUse(string email, SqlConnection regcon)
+        {
+            if (regcon.State != ConnectionState.Open)
+            {
+                regcon.Open();
+            }
+
+            using (SqlCommand checkEmailCmd = new SqlCommand("SELECT COUNT(*) FROM Employee WHERE Email = @Email", regcon))
+            {
+                checkEmailCmd.Parameters.AddWithValue("@Email", email);
+                int count = (int)checkEmailCmd.ExecuteScalar();
+                return count > 0;
+            }
+        }
+        private string GetMessageBasedOnErrorCode(int errorCode)
+        {
+            // Handle specific SQL error codes and return corresponding messages
+            if (errorCode == 2627 || errorCode == 2601)
+            {
+                return "Employee ID already exists. Please choose a different ID.";
+            }
+
+            // Add more cases as needed
+
+            // Default message
+            return "An error occurred.";
         }
 
         private void ClearInputFields()
